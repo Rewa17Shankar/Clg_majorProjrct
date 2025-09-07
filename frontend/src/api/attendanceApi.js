73cbec683b3d5214077f3068c744c3525ea3b32c
Rewa17Shankar <shankarewajii@gmail.com>
2025-09-07 16:09:22 +0530

    frontend

--- frontend/src/api/attendanceApi.js
+++ frontend/src/api/attendanceApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/attendance";
-
+// const API = "http://localhost:5000/api/attendance";
+const API = "https://clg-majorprojrct.onrender.com/api/attendance";
 export const markAttendance = (attendance) => axios.post(API, attendance);
 export const getAttendance = () => axios.get(API);
--- frontend/src/api/payrollApi.js
+++ frontend/src/api/payrollApi.js
@@ -13,8 +13,8 @@
 // frontend/src/api/payrollApi.js
 import axios from "axios";
 
-const API = "http://localhost:5000/api/payroll";
-
+// const API = "http://localhost:5000/api/payroll";
+const API = "https://clg-majorprojrct.onrender.com/api/payroll";
 export const getPayrolls = () => axios.get(API);
 export const generatePayroll = (data) => axios.post(`${API}/generate`, data); // <-- FIXED
 export const getPayrollByUser = (userId) => axios.get(`${API}/user/${userId}`);
