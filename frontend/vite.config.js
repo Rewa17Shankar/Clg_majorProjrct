6c8b06486d66705226dee740bdc8ff17c2d3a0b2
shrey sharma <sharmashrey6268@gmail.com>
2025-11-18 11:32:31 +0530

    Initial clean commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-=======
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
   server: {
-    historyApiFallback: true  // enable SPA fallback for client-side routing
+    historyApiFallback: true // enable SPA fallback for client-side routing
   }
->>>>>>> 148882b1f7e7cee9fa004438dfe529cb8cdc98b5
 })
